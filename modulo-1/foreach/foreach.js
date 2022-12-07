46745ddcf5bf312944f57dd34f82ad8c5619192c
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-07 20:10:55 -0300

    update

--- modulo-1/foreach/foreach.js
+++ modulo-1/foreach/foreach.js
@@ -1,6 +1,7 @@
 let frutas = ["banana", "uva", "maça"]
 
 // o index n é obrigatório, ele é útil quando for preciso saber a posição do array de cada item
+// forEach significa para cada
 frutas.forEach(function(fruta, index){
     console.log(`${index}: ${fruta}`)
 })
---
+++ modulo-1/map/map.js
@@ -0,0 +1,7 @@
+const nomes = ["maria", "joão", "josé"]
+
+const caixaAlta = nomes.map(function(nome){
+    return nome.toUpperCase()
+})
+
+console.log(caixaAlta)
