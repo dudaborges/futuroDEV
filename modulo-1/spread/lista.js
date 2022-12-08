9b3e3a570190f106153a661531cebcd9d47df0a7
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-08 20:58:41 -0300

    spread

---
+++ modulo-1/spread/lista.js
@@ -0,0 +1,6 @@
+let material = ['borracha', 'lápis']
+let pessoal = ['planner', 'jóia']
+let roupa = ['body', 'cropped']
+let listaCompras = [...material, ...pessoal, ...roupa]
+
+console.log(listaCompras)
