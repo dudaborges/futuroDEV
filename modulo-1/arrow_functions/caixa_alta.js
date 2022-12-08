b0792c351f2a8f807bc3e2def21329346c1ef719
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-08 19:45:51 -0300

    update

--- modulo-1/arrow_functions/caixa_alta.js
+++ modulo-1/arrow_functions/caixa_alta.js
@@ -1,1 +1,7 @@
 // callback functions
+
+const nomes = ["João", "Maria", "José"]
+
+const caixaAlta = nomes.map(nome => nome.toUpperCase())
+
+console.log(caixaAlta)
---
+++ modulo-1/arrow_functions/desafio.js
@@ -0,0 +1,10 @@
+// const dobro = () => {
+//     return `O dobro do número x é y`
+// }
+
+const dobro = (numero) => {
+    const dobroNumero = numero * 2
+    return `O dobro do número ${numero} é ${dobroNumero}`
+}
+
+console.log(dobro(2))
