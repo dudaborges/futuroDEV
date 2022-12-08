b5b33d8cb1bb132cc20a97e1042083d66ed88161
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-08 19:38:43 -0300

    arrow function

--- modulo-1/arrow_functions/arrow_functions2.js
+++ modulo-1/arrow_functions/arrow_functions2.js
@@ -3,3 +3,8 @@ const soma = (valorA, valorB) => {
 }
 
 console.log(soma(10, 5))
+
+//uma linha de return permite omitir as chaves
+// const soma = (valorA, valorB) => valorA + valorB
+
+
---
+++ modulo-1/arrow_functions/duvida.js
@@ -0,0 +1,9 @@
+let bemVindo = nome => {
+    return `Seja bem vind@ ${nome}`  
+}
+
+function bemVindo(nome){
+    return `Seja bem vind@ ${nome}` 
+}
+
+console.log(nome)
