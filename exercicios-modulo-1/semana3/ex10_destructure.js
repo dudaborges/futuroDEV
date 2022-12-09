1f365cf75aa4206e4f1f45929b396cb84585aa93
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-08 21:59:39 -0300

    destructure + function exercise

--- exercicios-modulo-1/semana3/ex10_destructure.js
+++ exercicios-modulo-1/semana3/ex10_destructure.js
@@ -1,9 +1,25 @@
+// let pet = {
+//     nome: "Jully",
+//     idade: 4,
+//     pelagem: 'branca'
+// }
+
+// let {nome, idade, pelagem} = pet
+
+// console.log(`O pet se chama ${nome}, tem ${idade} anos e pelagem ${pelagem}`)
+
+// com function, o ideal é não colocar console log dentro de uma função, já que o q tu vai fazer com o dado depois tu q define
+
 let pet = {
     nome: "Jully",
     idade: 4,
     pelagem: 'branca'
 }
 
-let {nome, idade, pelagem} = pet
+const apresentaPet = ({nome, idade, pelagem}) => {
+    return `O pet se chama ${nome}, tem ${idade} anos e pelagem ${pelagem}`
+}
 
-console.log(`O pet se chama ${nome}, tem ${idade} anos e pelagem ${pelagem}`)
+
+
+console.log(apresentaPet(pet))
