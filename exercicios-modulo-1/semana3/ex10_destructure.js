1ad37b0317b16a99ecd875ef73b7c1a761049431
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-08 21:54:36 -0300

    destructure exercise

---
+++ exercicios-modulo-1/semana3/ex10_destructure.js
@@ -0,0 +1,9 @@
+let pet = {
+    nome: "Jully",
+    idade: 4,
+    pelagem: 'branca'
+}
+
+let {nome, idade, pelagem} = pet
+
+console.log(`O pet se chama ${nome}, tem ${idade} anos e pelagem ${pelagem}`)
