9861a8b63e9776fd698c2bbde0b85d0d2eb33e54
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-12 19:29:14 -0300

    conditional (ternary) operator

---
+++ modulo-1/operador-ternario/copa.js
@@ -0,0 +1,12 @@
+let ganhou = "não"
+// if (ganhou == "sim") {
+//     console.log("brasil passou para semi")
+// } else {
+//     console.log("brasil foi eliminado")    
+// }
+
+// substituir o if else em menos linhas
+// ? é como se fosse as chaves, e : o primeiro é if e o segundo é else
+let resultado = (ganhou == "não") ? "brasil passou para semi" : "brasil foi eliminado"
+
+console.log(resultado)
