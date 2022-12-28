fd8cc0e090dce899f8f973c03fe1048896cb506e
Maria Eduarda Borges <duda.pborges92@gmail.com>
2022-12-27 21:14:31 -0300

    api + async

--- modulo-1/semana4/asyncAwait/api.js
+++ modulo-1/semana4/asyncAwait/api.js
@@ -3,9 +3,21 @@
 function getUser(id){
     // fetch é um recurso de requição
     // esse fetch retorna uma promise
-    return fetch(`https://regres.in/api/user?id=${id}`)
+    return fetch(`https://reqres.in/api/users?id=${id}`) 
     // o then está tratando o dado para a estrutura de JSON (aquela estrutura com os dados dentro de chaves, e organizados)
     // then recebe os dados do fetch
     .then((data) => data.json())
     .catch((err) => console.log('[ERROR]'+ err)) 
 }
+
+async function username(id){
+    try {
+        const user = await getUser(id)
+        console.log(`o nome de usuario é:${user.data.first_name}`)
+    } catch (error) {
+        console.log(`Erro: ${error}`)
+    }
+
+}
+
+username()
