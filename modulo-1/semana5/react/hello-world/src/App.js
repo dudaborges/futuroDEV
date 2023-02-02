9120379a607d836888242063eb2a65c8720cb099
Maria Eduarda Borges <duda.pborges92@gmail.com>
2023-02-02 18:20:46 -0300

    refactor

--- modulo-1/semana5/react/hello-world/src/App.js
+++ modulo-1/semana5/react/hello-world/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import PropsExample from './components/PropsExample';
 import TemplateExpressions from './components/TemplateExpressions';
-import OnClick from './components/OnClick';
-import OnChange from './components/OnChange';
-import OnSubmit from './components/OnSubmit';
-import OnKeyDown from './components/OnKeyDowm';
-import OnMouseOver from './components/OnMouseOver';
+import OnClick from './components/events/OnClick';
+import OnChange from './components/events/OnChange';
+import OnSubmit from './components/events/OnSubmit';
+import OnKeyDown from './components/events/OnKeyDowm';
+import OnMouseOver from './components/events/OnMouseOver';
 import Counter from './components/Counter';
 import RandomAdvice from './components/advice/RandomAdvice';
 import SearchAdvice from './components/advice/SearchAdvice';
-import Colors from './components/Colors';
+import Colors from './components/useState/Colors';
 
 function App() {
   const name = "Maria Eduarda"
