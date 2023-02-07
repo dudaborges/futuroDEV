f620bb57c808b9c8932e33e4f9f1523d1a744078
Maria Eduarda Borges <duda.pborges92@gmail.com>
2023-02-06 21:51:40 -0300

    class component

---
+++ modulo-1/semana7/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Class from './components/Class';
+
+function App() {
+  return (
+    <>
+    <Class />
+    </>
+  )
+}
+
+export default App;
---
+++ modulo-1/semana7/hello-world/src/components/Class.js
@@ -0,0 +1,28 @@
+import React from "react";
+// extends é um conceito de herança
+// o filho herda toda a herança do pai
+class Class extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.modelo = '2020'
+        // pra inicializar as variáveis
+        this.state = {
+            ligado: false,
+            velAtual: 0
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Meu onix</h1>
+                <p>Modelo {this.modelo}</p>
+                <p>Ligado {this.ligado}</p>
+                <p>velAtual {this.velAtual}</p>
+            </div>
+        )
+    }
+}
+
+export default Class
