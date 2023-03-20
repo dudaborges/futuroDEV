2fd5f0714fcfd1d1820e68384d37e5f7b2abccfe
dudaborges <duda.pborges92@gmail.com>
2023-03-19 21:44:45 -0300

    array manipulation exercise

---
+++ exercicios-modulo-2/semana1/src/Ex5_Name.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex5_Name {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = new String[5];
+        names[0] = "Maria";
+        names[1] = "João";
+        names[2] = "José";
+        names[3] = "Eduarda";
+        names[4] = "Pedro";
+
+        System.out.println("The first position is: " + names[0]);
+        System.out.println("The list is:");
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+    }
+}
